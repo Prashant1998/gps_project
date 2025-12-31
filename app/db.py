c67d11e54906596c074391d79379067e3d065710
Prashant Mishra <raviprashant.mishra@gmail.com>
2025-12-31 23:16:44 +0530

    Refactor: separate DB connection and SQL queries

--- app/db.py
+++ app/db.py
@@ -1,6 +1,16 @@
+import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_engine():
     return create_engine(
-        "postgresql+psycopg2://postgres:110868@localhost:5432/geolife"
+        f"postgresql+psycopg2://"
+        f"{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
+        f"{os.getenv('DB_NAME')}"
     )
+
